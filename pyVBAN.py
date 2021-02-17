d76cbabf82873f6565af89b7a459ed725c8feaa8
Samuel <tuglersamuel@gmail.com>
2021-02-17 01:32:25 +0100

    Fixed the module for python3

--- pyVBAN.py
+++ pyVBAN.py
@@ -11,6 +11,7 @@ class VBAN_Recv(object):
 		self.senderIp = senderIp
 		self.const_VBAN_SRList = [6000, 12000, 24000, 48000, 96000, 192000, 384000, 8000, 16000, 32000, 64000, 128000, 256000, 512000,11025, 22050, 44100, 88200, 176400, 352800, 705600] 
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+		self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 		self.sock.bind(("0.0.0.0", port))
 		self.sampRate = 48000
 		self.channels = 2
@@ -39,17 +40,17 @@ class VBAN_Recv(object):
 		self.stream = self.p.open(format = self.p.get_format_from_width(2), channels = self.channels, rate = self.sampRate, output = True, output_device_index=self.outDeviceIndex)
 
 	def _cutAtNullByte(self,stri):
-		return stri.split("\x00")[0]
+		return stri.decode('utf-8').split("\x00")[0]
 
 	def _parseHeader(self,data):
-		self.stream_magicString = data[0:4]
-		sampRateIndex = ord(data[4]) & 0x1F
-		self.subprotocol = (ord(data[4]) & 0xE0) >> 5
+		self.stream_magicString = data[0:4].decode('utf-8')
+		sampRateIndex = data[4] & 0x1F
+		self.subprotocol = (data[4] & 0xE0) >> 5
 		self.stream_sampRate = self.const_VBAN_SRList[sampRateIndex]
-		self.stream_sampNum = ord(data[5]) + 1
-		self.stream_chanNum = ord(data[6]) + 1
-		self.stream_dataFormat = ord(data[7])
-		self.stream_streamName = self._cutAtNullByte(''.join(struct.unpack("cccccccccccccccc",data[8:24])))
+		self.stream_sampNum = data[5] + 1
+		self.stream_chanNum = data[6] + 1
+		self.stream_dataFormat = data[7]
+		self.stream_streamName = self._cutAtNullByte(b''.join(struct.unpack("cccccccccccccccc",data[8:24])))
 		self.stream_frameCounter = struct.unpack("l",data[24:28])[0]
 
 	def runonce(self):
@@ -60,7 +61,7 @@ class VBAN_Recv(object):
 		self.rawData = data
 		self._parseHeader(data)
 		if self.verbose:
-			print "R"+self.stream_magicString+" "+str(self.stream_sampRate)+"Hz "+str(self.stream_sampNum)+"samp "+str(self.stream_chanNum)+"chan Format:"+str(self.stream_dataFormat)+" Name:"+self.stream_streamName+" Frame:"+str(self.stream_frameCounter)
+			print("R"+self.stream_magicString+" "+str(self.stream_sampRate)+"Hz "+str(self.stream_sampNum)+"samp "+str(self.stream_chanNum)+"chan Format:"+str(self.stream_dataFormat)+" Name:"+self.stream_streamName+" Frame:"+str(self.stream_frameCounter))
 		self.rawPcm = data[28:]   #Header stops a 28
 		if self.stream_magicString == "VBAN" and self.subprotocol == 0:
 			if not self.stream_streamName == self.streamName:
@@ -100,6 +101,7 @@ class VBAN_Send(object):
 		self.inDeviceIndex = inDeviceIndex
 		self.chunkSize = 256
 		self.stream = self.p.open(format=self.p.get_format_from_width(2), channels=self.channels,rate=self.samprate, input=True,input_device_index = self.inDeviceIndex, frames_per_buffer=self.chunkSize)
+
 		self.framecounter = 0
 		self.running = True
 		self.verbose = verbose
@@ -107,15 +109,15 @@ class VBAN_Send(object):
 		self.rawData = None
 
 	def _constructFrame(self,pcmData):
-		header  = "VBAN" 
-		header += chr(self.const_VBAN_SR.index(self.samprate))
-		header += chr(self.chunkSize-1)
-		header += chr(self.channels-1)
-		header += chr(1)  #VBAN_CODEC_PCM
-		header += self.streamName + "\x00" * (16 - len(self.streamName))
+		header  = b"VBAN" 
+		header += bytes([self.const_VBAN_SR.index(self.samprate)])
+		header += bytes([self.chunkSize-1])
+		header += bytes([self.channels-1])
+		header += b'\x01'  #VBAN_CODEC_PCM
+		header += bytes(self.streamName + "\x00" * (16 - len(self.streamName)), 'utf-8')
 		header += struct.pack("l",self.framecounter)
 		if self.verbose:
-			print "SVBAN "+str(self.samprate)+"Hz "+str(self.chunkSize)+"samp "+str(self.channels)+"chan Format:1 Name:"+self.streamName+" Frame:"+str(self.framecounter)
+			print("SVBAN "+str(self.samprate)+"Hz "+str(self.chunkSize)+"samp "+str(self.channels)+"chan Format:1 Name:"+self.streamName+" Frame:"+str(self.framecounter))
 		return header+pcmData
 
 	def runonce(self):
@@ -124,8 +126,8 @@ class VBAN_Send(object):
 			self.rawPcm = self.stream.read(self.chunkSize)
 			self.rawData = self._constructFrame(self.rawPcm)
 			self.sock.sendto(self.rawData, (self.toIp,self.toPort))
-		except:
-			pass
+		except Exception as e:
+			print(e)
 
 	def runforever(self):
 		while self.running:
@@ -135,7 +137,6 @@ class VBAN_Send(object):
 		self.running = False
 		self.stream.close()
 		self.stream = None
-
 
 class VBAN_SendText(object):
 	"""docstring for VBAN_SendText"""
@@ -151,14 +152,14 @@ class VBAN_SendText(object):
 		self.framecounter = 0
 
 	def _constructFrame(self,text):
-		header  = "VBAN" 
-		header += chr(int("0b01000000",2)  + self.VBAN_BPSList.index(self.baudRate))
-		header += chr(int("0b00000000",2))
-		header += chr(int("0b00000000",2))
-		header += chr(int("0b00010000",2)) # UTF8
-		header += self.streamName + "\x00" * (16 - len(self.streamName))
+		header  = b"VBAN" 
+		header += bytes([int("0b01000000",2)  + self.VBAN_BPSList.index(self.baudRate)])
+		header += b'\x00'
+		header += b'\x00' #Channel indent 0 by default
+		header += bytes([int("0b00010000",2)]) # UTF8
+		header += bytes(self.streamName + "\x00" * (16 - len(self.streamName)), 'utf-8')
 		header += struct.pack("l",self.framecounter)
-		return header+text
+		return header+bytes(text, 'utf-8')
 
 	def send(self,text):
 		try:
@@ -166,4 +167,4 @@ class VBAN_SendText(object):
 			self.rawData = self._constructFrame(text)
 			self.sock.sendto(self.rawData, (self.toIp,self.toPort))
 		except Exception as e:
-			pass
+			print(e)
